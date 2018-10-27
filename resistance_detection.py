d24693fed6babb7f9f5de2dc3a3a1fadb65efca1
jpro <jpro6679@naver.com>
2018-10-27 23:00:57 +0900

    2018.10.27

--- resistance_detection.py
+++ resistance_detection.py
@@ -1,59 +1,64 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
-#엣지 검출할 그레이사진 하나, BGR값 찾을 칼라사진 하나
-img = cv2.imread('all.jpg')
-img2 = cv2.imread('all.jpg',0)
-img3 = img2 + img2 + img2
-img4 = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-img5 = img4 - img4 - img4
-img6 = cv2.cvtColor(img5,cv2.COLOR_HSV2BGR)
-img7 = cv2.cvtColor(img6,cv2.COLOR_BGR2GRAY)
+#이미지 불러오기
+img = cv2.imread('C:/Users/jhm/Desktop/kopo_univ/all.JPG')
+img_rgb = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+rows, cols, _ = img.shape
+print("rows:",rows), print("cols:",cols)
+# #관심영역 자르기
+# roi = img_rgb[0:rows, 0:cols]
+#회색 변환
+img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-#choice1. sobel -> 탈락
-# kernel = np.ones((3,3),np.float32)/9
-# dst = cv2.filter2D(img2,-1,kernel)
-# kernel2 = np.ones((5,5),np.float32)/25
-# dst2 = cv2.filter2D(img2,-1,kernel2)
-# kernel3 = np.ones((7,7),np.float32)/49
-# dst3 = cv2.filter2D(img2,-1,kernel3)
-#
-# sobelx = cv2.Sobel(dst3,cv2.CV_64F,1,0,ksize=3)
-# sobelx2 = cv2.Sobel(dst3,cv2.CV_64F,1,0,ksize=5)
-# sobelx3 = cv2.Sobel(dst3,cv2.CV_64F,1,0,ksize=7)
-# sobely = cv2.Sobel(dst3,cv2.CV_64F,0,1,ksize=3)
-# sobely2 = cv2.Sobel(dst3,cv2.CV_64F,0,1,ksize=5)
-# sobely3 = cv2.Sobel(dst3,cv2.CV_64F,0,1,ksize=7)
-#
-# j = sobelx + sobely
-# j2 = sobelx2 + sobely2
-# j3 = sobelx3 + sobely3
+#이미지 이진화로 저항의 몸체만 추출
+_, img_bin = cv2.threshold(img_gray,240,255,cv2.THRESH_BINARY_INV)
 
-#choice2. laplacian
-# kernel = np.ones((3,3),np.float32)/9
-# dst = cv2.filter2D(img2,-1,kernel)
-# kernel2 = np.ones((5,5),np.float32)/25
-# dst2 = cv2.filter2D(img2,-1,kernel2)
-# kernel3 = np.ones((7,7),np.float32)/49
-# dst3 = cv2.filter2D(img2,-1,kernel3)
-#
-# j = cv2.Laplacian(dst,cv2.CV_64F)
-# j2 = cv2.Laplacian(dst2,cv2.CV_64F)
-# j3 = cv2.Laplacian(dst3,cv2.CV_64F)
+i=0
+j=0
+my_xy = []
+#왼쪽상단 꼭지점 구하기
+print("hi",img_bin[530][273])
+##################
+for r in range(rows):
+    for c in range(cols):
+        if img_bin[r][c] == 255:
+            left_top_x = c
+            left_top_y = r
+            print("bye", left_top_x, left_top_y)
+            my_xy.append([left_top_x,left_top_y])
+            print("zzz",my_xy[0])
 
-#Choice3. Canny Edge Detection
-edges = cv2.Canny(img4, 100, 200)
-print(edges)
 
-#result.
-cv2.imshow('img3',img3)
-cv2.imshow('img4',img4)
-cv2.imshow('img2',img2)
-cv2.imshow('edges',edges)
-cv2.imshow('img5',img5)
-cv2.imshow('img7',img7)
 
-# cv2.imshow('dst',j)
-# cv2.imshow('dst2', j2)
-# cv2.imshow('dst3', j3)
-cv2.waitKey(0)
+#########################
+# #오른쪽상단 꼭지점 구하기
+# for i in range(cols):
+#     for j in range(rows):
+#         if img_bin[i][273-j] is 255:
+#             right_top = 273-j
+#             print(right_top)
+#             break
+
+# #저항 몸체 라벨링
+# _, contours, _ = cv2.findContours(img_bin,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+# cv2.drawContours(img_rgb, contours, -1, (0,255,0), 5)
+
+
+#사각형 그리기
+#cv2.Rectangle(image, (top, left), (bottom, right), (B, G, R), thickness)
+# img_mask = cv2.rectangle(img_bin,(384,0),(510,128),(0,255,0),3)
+plt.subplot(1,2,1), plt.imshow(img_rgb), plt.title('img_rgb')
+plt.subplot(1,2,2), plt.imshow(img_bin), plt.title('img_bin')
+plt.show()
+
+
+
+#결과 확인
+# cv2.imshow('img_bin',img_bin)
+
+
+
+
+
